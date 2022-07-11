7c977cd332905b38e4fc8ea7b246422cf4612dd6
Muhammad Athallah <mhd.athallah@gmail.com>
2022-07-11 14:03:07 +0700

    Migrate alarm plugin to plus version

--- submission_final/lib/main.dart
+++ submission_final/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- submission_final/lib/provider/scheduling_provider.dart
+++ submission_final/lib/provider/scheduling_provider.dart
@@ -1,4 +1,4 @@
-import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:submission_final/utils/background_service.dart';
 import 'package:submission_final/utils/date_time_helper.dart';
